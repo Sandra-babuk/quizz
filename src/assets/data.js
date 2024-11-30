cf19c48d1798c9b2d6575f4933b7ce289fc8480d
Sandra-babuk <sandrababu73@gmail.com>
2024-11-30 09:38:50 +0530

    version1

---
+++ src/assets/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+      question: "Which planet is known as the Red Planet?",
+      option1: "Venus",
+      option2: "Jupiter",
+      option3: "Saturn",
+      option4: "Mars",
+      ans: 4,
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      option1: "Atlantic Ocean",
+      option2: "Indian Ocean",
+      option3: "Pacafic Ocean",
+      option4: "Arctic Ocean",
+      ans: 3,
+    },
+    {
+      question: " Who wrote the play Romeo and Juliet?",
+      option1: "William Shakespeare",
+      option2: "Charles Dickens",
+      option3: "Mark Twain",
+      option4: "J.K. Rowling",
+      ans: 1,
+    },
+    {
+      question: " What is the chemical symbol for water?",
+      option1: "CO2",
+      option2: "H2O",
+      option3: " O2",
+      option4: " NaCl",
+      ans: 2,
+    },
+    {
+        question: " Which gas do plants absorb from the atmosphere during photosynthesis?",
+        option1: "Oxygen",
+        option2: "Nitrogen",
+        option3: "  Carbon dioxide",
+        option4: " Hydrogen",
+        ans: 3
+
+    }
+   
+ 
+   
+];
